59c2920c1c7e27b704d5f2b359a2a2ee5df0a0ea
Tringapps_rajuswami <123533864+rajuswami123@users.noreply.github.com>
2023-03-15 17:28:11 +0530

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Header(props) {
+  return (
+    <header className="block row center">
+      <div>
+        <a href="#/" className="badges">
+          <h1>Mens Wear</h1>
+        </a>
+      </div>
+      <div>
+        <a href="#/cart" className="badges">
+          Cart{' '}
+          {props.countCartItems ? (
+            <button className="badge">{props.countCartItems}</button>
+          ) : (
+            ''
+          )}
+        </a>{' '}
+        
+      </div>
+    </header>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,25 @@
+
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Kurthi',
+        price: 1400,
+        image: 'https://image.menswearhouse.com/is/image/TMW/MW40_3X01_06_WILKE_RODRIGUEZ_BURGUNDY_TIC_MAIN?$40GridLrg$',
+      },
+      {
+        id: '2',
+        name: 'Old Piece',
+        price: 2400,
+        image: 'https://image.menswearhouse.com/is/image/TMW/MW40_12V9_02_PRONTO_UOMO_BLACK_SOLID_BT_MAIN?$TRDSlide$',
+      },
+      {
+        id: '3',
+        name: 'Jacket',
+        price: 5000,
+        image: 'https://image.menswearhouse.com/is/image/TMW/MW40_3X1C_64_PRONTO_UOMO_PLATINUM_GRAY_PLAID_MAIN?$40GridLrg$',
+      },
+      
+    ],
+  };
+  export default data;
